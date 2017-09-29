05af466af7d39a602675f0cd1faf1dfed849ba20
Valentin Brosseau <c4software@gmail.com>
2017-09-29 22:25:04 +0200

    Ajout texte dans l’écran about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <v-container>
         <h2>About this app</h2>
         <p>
             This is a very simple app that uses basic physics to tell you the height of a cliff or the depth of a well. All you do is time how long it takes for the object to hit the ground and let science do the rest. You can even measure the height of a cliff from the ground if you throw a rock right up to the top and start timing at the apex.
@@ -7,7 +7,7 @@
         <p>
             This app was inspired by Science/Design/Creativity YouTuber Mark Rober.
         </p>
-    </div>
+    </v-container>
 </template>
 
 <script>
