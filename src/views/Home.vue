6820101855fde00cda4a010f410516c7829e8b2c
Valentin Brosseau <c4software@gmail.com>
2017-09-29 23:01:24 +0200

    Ajout action start / stop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
     </div>
 
     <v-fab-transition>
-      <v-btn v-bind:class="[isRunning?'red':'green']" dark fixed bottom right fab>
-        <v-icon>play_arrow</v-icon>
+      <v-btn v-on:click="action" v-bind:class="state_class" dark fixed bottom right fab>
+        <v-icon>{{ state_icon }}</v-icon>
       </v-btn>
     </v-fab-transition>
   </div>
@@ -16,7 +16,58 @@
 export default {
     name: 'home',
     data: () => {
-        return {isRunning: false}
+        return {
+            running: false,
+            time: 0,
+            m: "0",
+            ft: "0"
+        }
+    },
+    computed:{
+      state_icon() {
+          if(this.running){
+              return "stop";
+          }else{
+              return "play_arrow";
+          }
+      },
+      state_class() {
+          if(this.running){
+              return "red";
+          }else{
+              return "green";
+          }
+      }
+    },
+    methods:{
+        action(){
+            this.start = new Date().getTime();
+
+            if (this.running === false){
+                console.log("Start");
+                this.running = true;
+                this.timerId = setInterval(() => {
+                    this.compute();
+                }, 10);
+            }else{
+                console.log("Stop");
+                this.running = false;
+                clearInterval(this.timerId);
+            }
+
+        },
+        compute(){
+            let fallTime = new Date().getTime() - this.start;
+            let height = 16*Math.pow((fallTime/1000), 2);
+            let numberDigits = 1;
+            if(height < 1){
+                numberDigits = 2;
+            }
+
+            this.time   = (fallTime/1000).toFixed(2);
+            this.ft     = height.toFixed(numberDigits);
+            this.m      = (height/3.2808).toFixed(numberDigits);
+        }
     }
 }
 </script>
