2d35993edcd5fe7c5a93fc9f250b72646afc53e8
Valentin Brosseau <c4software@gmail.com>
2017-09-29 22:31:14 +0200

    Ajout texte FR/EN

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,39 +3,49 @@ import Vue from 'vue'
 Vue.use(VueI18n);
 
 const messages = {
-  en: {
-    title: 'Cliff Height Timer',
-    openMenu: 'Open Menu',
-    drawer: {
-      home: "Home",
-      about: "About"
+    en: {
+        title: 'Cliff Height Timer',
+        openMenu: 'Open Menu',
+        drawer: {
+            home: "Home",
+            about: "About"
+        },
+        see_on_github: "See On Github",
+        about: {
+          title: "About this app",
+          p1: "This is a very simple app that uses basic physics to tell you the height of a cliff or the depth of a well. All you do is time how long it takes for the object to hit the ground and let science do the rest. You can even measure the height of a cliff from the ground if you throw a rock right up to the top and start timing at the apex.",
+          p2: "This app was inspired by Science/Design/Creativity YouTuber Mark Rober."
+        }
     },
-    see_on_github: "See On Github"
-  },
-  fr: {
-    title: "Cliff Height Timer",
-    openMenu: 'Ouvrir le Menu',
-    drawer: {
-      home: "Accueil",
-      about: "À propos"
-    },
-    see_on_github: "Voir sur Github"
-  }
+    fr: {
+        title: "Cliff Height Timer",
+        openMenu: 'Ouvrir le Menu',
+        drawer: {
+            home: "Accueil",
+            about: "À propos"
+        },
+        see_on_github: "Voir sur Github",
+        about: {
+            title: "À Propos",
+            p1: "Il s'agit d'une application très simple qui utilise la physique de base pour vous dire la hauteur d'une falaise ou la profondeur d'un puits. Tout ce que vous avez à faire, c'est de compter le temps qu’il faut pour que l'objet frappe le sol et laisser la science faire le reste.",
+            p2: "Cette application a été inspirée par le YouTuber Mark Rober."
+        }
+    }
 };
 
 
 export const i18n = new VueI18n({
-  locale: getLanguage(),
-  fallbackLocale: 'en',
-  messages,
+    locale: getLanguage(),
+    fallbackLocale: 'en',
+    messages,
 });
 
 function getLanguage() {
-  try {
-    let l = navigator.language.split("-");
-    return l[0];
-  }catch(err){
-    console.log(err);
-    return navigator.language;
-  }
+    try {
+        let l = navigator.language.split("-");
+        return l[0];
+    }catch(err){
+        console.log(err);
+        return navigator.language;
+    }
 }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,8 @@
 <template>
     <v-container>
-        <h2>About this app</h2>
-        <p>
-            This is a very simple app that uses basic physics to tell you the height of a cliff or the depth of a well. All you do is time how long it takes for the object to hit the ground and let science do the rest. You can even measure the height of a cliff from the ground if you throw a rock right up to the top and start timing at the apex.
-        </p>
-        <p>
-            This app was inspired by Science/Design/Creativity YouTuber Mark Rober.
-        </p>
+        <h2>{{ $t("about.title") }}</h2>
+        <p>{{ $t("about.p1") }}</p>
+        <p>{{ $t("about.p2") }}</p>
     </v-container>
 </template>
 
