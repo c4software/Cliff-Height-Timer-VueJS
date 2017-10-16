1e8c6e45a6c2a0b44ffe5e515fb99e1b15ec6032
Valentin Brosseau <c4software@gmail.com>
2017-10-16 21:35:57 +0200

    Fix export

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -44,11 +44,7 @@ const messages = {
 };
 
 
-export const i18n = new VueI18n({
-    locale: getLanguage(),
-    fallbackLocale: 'en',
-    messages
-});
+export const i18n = new VueI18n({locale: getLanguage(), fallbackLocale: 'en', messages});
 
 function getLanguage() {
     try {
