51eb8c7f7855b9cd9fe526e991bc55116d4ce65e
Valentin Brosseau <c4software@gmail.com>
2017-12-08 16:01:10 +0100

    Correction Lint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,76 +7,79 @@
     </div>
 
     <v-btn v-on:click="action" v-bind:class="state_class" dark fixed bottom right fab>
-        <v-icon>{{ state_icon }}</v-icon>
+      <v-icon>{{ state_icon }}</v-icon>
     </v-btn>
   </v-container>
 </template>
 
 <script>
-export default {
-    name: 'home',
-    data: () => {
-        return {
-            running: false,
-            time: 0,
-            m: "0",
-            ft: "0"
-        }
-    },
-    computed:{
-      state_icon() {
-          if(this.running){
-              return "stop";
-          }else{
-              return "play_arrow";
-          }
-      },
-      state_class() {
-          if(this.running){
-              return "red";
-          }else{
-              return "green";
-          }
-      }
-    },
-    methods:{
-        action(){
-            this.start = new Date().getTime();
-
-            if (!this.running){
-                this.running = true;
-                this.timerId = setInterval(() => {
-                    this.compute();
-                }, 10);
-            }else{
-                this.running = false;
-                clearInterval(this.timerId);
-                this.saveHistory({time: this.time,m: this.m,ft: this.ft});
+    export default {
+        name: 'home',
+        data: () => {
+            return {
+                running: false,
+                time: 0,
+                m: '0',
+                ft: '0'
             }
-
         },
-        saveHistory(item){
-          let history = JSON.parse(localStorage.getItem("history"));
-          if (!Array.isArray(history)){
-            history = [];
-          }
-          history.unshift(item);
-          localStorage.setItem("history", JSON.stringify(history));
-        },
-        compute(){
-            let fallTime = new Date().getTime() - this.start;
-            let height = 16*Math.pow((fallTime/1000), 2);
-            let numberDigits = 1;
-            if(height < 1){
-                numberDigits = 2;
+        computed: {
+            state_icon () {
+                // Retourne l’icone pour le FAB
+                if (this.running) {
+                    return 'stop'
+                } else {
+                    return 'play_arrow'
+                }
+            },
+            state_class () {
+                // Retourne la class pour le FAB
+                if (this.running) {
+                    return 'red'
+                } else {
+                    return 'green'
+                }
             }
+        },
+        methods: {
+            action () {
+                // Gestion du start / stop
+                this.start = new Date().getTime()
 
-            this.time   = (fallTime/1000).toFixed(2);
-            this.ft     = height.toFixed(numberDigits);
-            this.m      = (height/3.2808).toFixed(numberDigits);
+                if (!this.running) {
+                    this.running = true
+                    this.timerId = setInterval(() => {
+                        this.compute()
+                    }, 10)
+                } else {
+                    this.running = false
+                    clearInterval(this.timerId)
+                    this.saveHistory({time: this.time, m: this.m, ft: this.ft})
+                }
+            },
+            saveHistory (item) {
+                // Sauvegarde dans l’historique
+                let history = JSON.parse(localStorage.getItem('history'))
+                if (!Array.isArray(history)) {
+                    history = []
+                }
+                history.unshift(item)
+                localStorage.setItem('history', JSON.stringify(history))
+            },
+            compute () {
+                let fallTime = new Date().getTime() - this.start
+                let height = 16 * Math.pow((fallTime / 1000), 2)
+                let numberDigits = 1
+                if (height < 1) {
+                    numberDigits = 2
+                }
+
+                this.time = (fallTime / 1000).toFixed(2)
+                this.ft = height.toFixed(numberDigits)
+                this.m = (height / 3.2808).toFixed(numberDigits)
+            }
         }
     }
-}
 </script>
 
 <style scoped>
