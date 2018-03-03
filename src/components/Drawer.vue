be90e05aa1adeb84f9b34b055f0da5ffc0280844
Valentin Brosseau <c4software@gmail.com>
2018-03-03 21:22:36 +0100

    Update dependancies

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <v-toolbar class="deep-purple" />
     <v-list dense>
       <template v-for="(item, i) in items">
         <v-divider dark v-if="item.divider" :key="i"></v-divider>
