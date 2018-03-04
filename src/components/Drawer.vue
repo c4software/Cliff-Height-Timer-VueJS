fc2ff18e4c18b131eae3f63915f03b5f63b59f84
Valentin Brosseau <c4software@gmail.com>
2018-03-04 19:46:03 +0100

    Change :href => :to

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -4,7 +4,7 @@
     <v-list dense>
       <template v-for="(item, i) in items">
         <v-divider dark v-if="item.divider" :key="i"></v-divider>
-        <v-list-tile :href="item.action" :key="i" v-else>
+        <v-list-tile :to="item.action" :key="i" v-else>
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
